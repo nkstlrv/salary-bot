b005f45de1f319c8285fbfc78a8cc6a00c10518a
Nikita <stlrvnk@gmail.com>
2023-09-06 21:42:06 +0300

    basic hours

---
+++ salary.py
@@ -0,0 +1,10 @@
+def calculate_salary(total_hours: float, b_hours: float, beyond_b_hours: float, working_days: int) -> str:
+    obligatory_hours: int = 6
+
+    basic_hours: float = obligatory_hours * working_days
+    
+
+
+
+if __name__ == "__main__":
+    calculate_salary(total_hours=195.98, b_hours=183.98, beyond_b_hours=11.99, working_days=23)
