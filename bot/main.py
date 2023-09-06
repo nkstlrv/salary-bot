e3254508474a6565d4df18885bad82debcba07de
Nikita <stlrvnk@gmail.com>
2023-09-06 22:56:22 +0300

    telegram bot salary return

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 import time
 import logging
+from salary import calculate_salary
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
@@ -22,10 +23,56 @@ async def start(message: types.Message):
     await message.answer("This is <b>Salary</b> bot 🤖", parse_mode="HTML")
     time.sleep(1)
     await message.answer(
-        "I can help you with calculating your monthly salary",
+        "I can help you with calculating your <b>monthly salary</b>",
         parse_mode="HTML",
     )
     time.sleep(1)
+    await message.answer("To calculate your salary I need following data:", parse_mode="HTML")
+    time.sleep(1)
+    await message.answer("1) Total hours you worked this month \n"
+                         "2) Total Business hours you worked\n"
+                         "3) Total hours you worked Beyond Business Hours\n"
+                         "4) Working days\n"
+                         "5) Your Fixed salary", parse_mode="HTML")
+    time.sleep(1)
+    await message.answer("Please, provide it in such format", parse_mode="HTML")
+    await message.answer(
+        "<i>total_hours<b>|</b>business_hours<b>|</b>beyond_b_hours<b>|</b>working_days<b>|</b>fixed_salary</i>\n\n"
+        "For example:\n"
+        "195.98|183.98|11.99|23|500", parse_mode="HTML")
+
+
+@dp.message_handler(content_types=["text"])
+async def salary_calculation(message: types.Message):
+    if "|" in message.text:
+        query: list = [param.strip() for param in message.text.split("|")]
+
+        try:
+            total_hours = float(query[0])
+            business_hours = float(query[1])
+            beyond_b_hour = float(query[2])
+            working_days = int(query[3])
+            fixed_salary = int(query[4])
+
+            result = calculate_salary(total_hours, business_hours, beyond_b_hour, working_days, fixed_salary)
+
+            salary = "{:.2f}".format(result['salary'])
+            b_hours_bonuses = "{:.2f}".format(result['business'])
+            beyond_b_hours_bonuses = "{:.2f}".format(result['beyond'])
+
+            await message.answer("Processing...")
+            time.sleep(1)
+            await message.answer(f"Salary: <b>${salary}</b>\n\n"
+                                 f"Business hours bonuses: <b>${b_hours_bonuses}</b>\n"
+                                 f"Beyond business hours bonuses: <b>${beyond_b_hours_bonuses}</b>", parse_mode="html")
+
+        except IndexError:
+            await message.answer("❗ Invalid query format. Try again")
+
+        except ValueError:
+            await message.answer("❗ Invalid query format. Try again")
+    else:
+        await message.answer("❗ Invalid query format. Try again")
 
 
 if __name__ == "__main__":
--- bot/salary.py
+++ bot/salary.py
@@ -1,5 +1,4 @@
 def calculate_salary(total_hours: float, b_hours: float, beyond_b_hours: float, working_days: int, fixed_salary: int):
-    
     obligatory_hours: int = 6
     per_business_hour: int = 5  # $
     per_beyond_business_hour: int = 15  # $
@@ -15,15 +14,21 @@ def calculate_salary(total_hours: float, b_hours: float, beyond_b_hours: float, 
     # Bonus calculation:
     bonus_days: float = working_days - days_covered_by_basic
     bonus_for_b_hours: float = (
-        average_hours_per_day_business * bonus_days * per_business_hour
+            average_hours_per_day_business * bonus_days * per_business_hour
     )
     bonus_for_beyond_b_hours: float = (
-        average_hours_per_day_beyond_business * bonus_days * per_beyond_business_hour
+            average_hours_per_day_beyond_business * bonus_days * per_beyond_business_hour
     )
 
     total_monthly_salary: float = fixed_salary + bonus_for_b_hours + bonus_for_beyond_b_hours
 
-    return total_monthly_salary
+    return {
+
+        "business": bonus_for_b_hours,
+        "beyond": bonus_for_beyond_b_hours,
+        "salary": total_monthly_salary
+    }
+
 
 if __name__ == "__main__":
     calculate_salary(
